7dcc6d146066f2fadba2b38e8c8aa705211bc146
sampyxis <sampyxis@4fafe0fb-6237-5464-ab3f-925d16043ff4>
2010-09-18 02:59:17 +0000

    Totally re-did how this is going to work.
    This is now the start - the pde file (the Arduino file) is just a stub to start the rest of it off.
    
    Also - I have the start of a sample game here (Asteroids) - a very very simple asteroids at this point.

---
+++ example_game_file.cpp
@@ -0,0 +1,17 @@
+//
+//
+// Simple program to test
+// Take a bitmap and run through the joystick functions
+//
+//
+#include "game_engine.h"
+
+// Player
+static unsigned char __attribute__ ((progmem)) player_bmp[]= {
+0x30, 0x78, 0xFE, 0xC7, 0xC7, 0xFE, 0x78, 0x30 
+        };
+        
+
+void mainLoop1()
+{
+}
